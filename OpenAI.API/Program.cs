94be34cf4f2b667289927e68cc57c187e8dfbca8
oğuzcan kandemir <oguzcankandemir@hotmail.com>
2023-10-31 03:49:22 +0300

    [feature]

--- OpenAI.API/Program.cs
+++ OpenAI.API/Program.cs
@@ -1,9 +1,12 @@
-using OpenAI.API;
+using OpenAI.API.Example.Services;
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddOpenAIService(settings => settings.ApiKey = "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");
+        services.AddHostedService<OpenAICompletionService>();
     })
     .Build();
 
